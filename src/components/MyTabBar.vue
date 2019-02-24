9ac82c95483cc4fe2e4ad1b0e80e6b5982f1a1b1
agileabc <agileabc@sina.com>
2019-02-24 23:17:33 +0800

    Fix css issue.

--- src/components/MyTabBar.vue
+++ src/components/MyTabBar.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="dynamic-component-demo" class="demo">
+	<div id="dynamic-component-demo" class="dynamic-component-demo">
 		<button
 			v-for="tab in tabs"
 			v-bind:key="tab"
@@ -39,3 +39,29 @@ export default {
 	}
 }
 </script>
+
+<style scoped>
+.dynamic-component-demo {
+	text-align: left;
+}
+.tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+}
+.tab-button:hover {
+  background: #e0e0e0;
+}
+.tab-button.active {
+  background: #e0e0e0;
+}
+.tab {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
