4977d41f63c0a6f4ea22814218922dbfb0d68e2f
agileabc <agileabc@sina.com>
2019-07-05 17:49:32 +0800

    Add code.

--- src/app-other.vue
+++ src/app-other.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app-other">
-    other
+    first slot A: <br />
+    <FirstSlot>
+      Execuse Me: {{this.slotValue}}
+    </FirstSlot>
+    first slot B: <br />
+    <FirstSlot />
   </div>
 </template>
 
 <script>
+import FirstSlot from './components/first-slot.vue'
 
 export default {
-  name: 'AppOther'
+  name: 'AppOther',
+  components: {
+    FirstSlot
+  },
+	data: () => {
+		return {
+			slotValue: 'slot value...'
+		}
+	}
 }
 </script>
 
---
+++ src/components/first-slot.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>first slot: 
+		<slot>Default Value for slot</slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'FirstSlot'
+}
+</script>
+
+<style>
+
+</style>
