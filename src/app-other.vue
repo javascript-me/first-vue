971b0a125a13991ba3bf8d867389037ebd3cf11b
agileabc <agileabc@sina.com>
2019-07-16 23:29:13 +0800

    Update wording.

--- src/app-other.vue
+++ src/app-other.vue
@@ -9,14 +9,14 @@
     <hr>
     <BaseLayout>
       <template v-slot:header>
-        <h1>Here might be a page title</h1>
+        <h1>我是一个title</h1>
       </template>
 
       <p>A paragraph for the main content.</p>
       <p>And another one.</p>
 
       <template v-slot:footer>
-        <p>Here's some contact info</p>
+        <p>我是一个Footer</p>
       </template>
     </BaseLayout>
   </div>
