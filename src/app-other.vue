13a9bca2487e2d2752fb1d5162f5d52191556418
agileabc <agileabc@sina.com>
2019-07-17 00:00:36 +0800

    Woring solution.

--- src/app-other.vue
+++ src/app-other.vue
@@ -12,25 +12,35 @@
         <h1>我是一个title</h1>
       </template>
 
-      <p>A paragraph for the main content.</p>
-      <p>And another one.</p>
+      <template v-slot:default>
+        <p>我是主体内容。。。</p>
+        <p>A paragraph for the main content.</p>
+        <p>And another one.</p>
+      </template>
 
       <template v-slot:footer>
         <p>我是一个Footer</p>
       </template>
     </BaseLayout>
+
+    <hr>
+    <current-user>
+      <template v-slot:default>okok</template>
+    </current-user>
   </div>
 </template>
 
 <script>
 import FirstSlot from "./components/first-slot.vue";
 import BaseLayout from "./components/base-layout.vue";
+import CurrentUser from "./components/current-user.vue";
 
 export default {
   name: "AppOther",
   components: {
     FirstSlot,
-    BaseLayout
+    BaseLayout,
+    CurrentUser
   },
   data: () => {
     return {
---
+++ src/components/current-user.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>
+    <slot v-bind:user="user">{{ user.lastName }}</slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "CurrentUser",
+  data: () => {
+	  return {
+		  user: {
+			  lastName: 'chen',
+			  firstName: 'anthony'
+		  }
+	  }
+  }
+};
+</script>
+
+<style>
+</style>
