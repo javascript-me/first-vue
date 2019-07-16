d044120cb5ab17f49e70decc4e7811813ed98c3c
agileabc <agileabc@sina.com>
2019-07-16 23:27:18 +0800

    Add base layout component.

--- src/app-other.vue
+++ src/app-other.vue
@@ -1,30 +1,43 @@
 <template>
   <div id="app-other">
-    first slot A: <br />
-    <FirstSlot>
-      Execuse Me: {{this.slotValue}}
-    </FirstSlot>
-	<hr />
-    first slot B: <br />
-    <FirstSlot />
-	<hr />
+    first slot A:
+    <br>
+    <FirstSlot>Execuse Me: {{this.slotValue}}</FirstSlot>
+    <hr>first slot B:
+    <br>
+    <FirstSlot/>
+    <hr>
+    <BaseLayout>
+      <template v-slot:header>
+        <h1>Here might be a page title</h1>
+      </template>
+
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+
+      <template v-slot:footer>
+        <p>Here's some contact info</p>
+      </template>
+    </BaseLayout>
   </div>
 </template>
 
 <script>
-import FirstSlot from './components/first-slot.vue'
+import FirstSlot from "./components/first-slot.vue";
+import BaseLayout from "./components/base-layout.vue";
 
 export default {
-  name: 'AppOther',
+  name: "AppOther",
   components: {
-    FirstSlot
+    FirstSlot,
+    BaseLayout
   },
-	data: () => {
-		return {
-			slotValue: 'slot value...'
-		}
-	}
-}
+  data: () => {
+    return {
+      slotValue: "slot value..."
+    };
+  }
+};
 </script>
 
 <style>
