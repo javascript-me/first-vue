c3125cf283006d1e750dc72603661447d17b8e05
agileabc <agileabc@sina.com>
2019-08-13 10:38:49 +0800

    Programming hook.

--- src/components/my-tab-bar/tab-home.vue
+++ src/components/my-tab-bar/tab-home.vue
@@ -13,6 +13,13 @@ export default {
 		console.log('TabHome created. ')
 		console.log('this.$parent.tabs', this.$parent.tabs)
 		console.log('this.$parent.currentTab', this.$parent.currentTab)
+
+		this.$once('hook:beforeDestroy', function () {
+			// 由于my-tab-bar.vue里面使用了<keep-alive>标签，所以只有切换路由，
+			// 如从【http://localhost:8080/】跳转到【http://localhost:8080/app-other】
+			// 才会触发以下代码的运行。
+			console.log('tab home destroy...')
+		})
 	},
 	methods: {
 		callParentMethod: function () {
