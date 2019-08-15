46ec5ecba5a20f02f432f3afa04c5a9d23373c15
agileabc <agileabc@sina.com>
2019-08-15 17:06:48 +0800

    Update unit test.

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,15 @@
-import { expect } from 'chai'
+import { expect, assert } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
+	it('renders props.msg when passed', () => {
+		const message = 'new message'
+		const wrapper = shallowMount(HelloWorld, {
+			propsData: { msg: message }
+		})
+
+		assert.ok(wrapper.text().indexOf(message) !== -1)
+		expect(wrapper.text()).to.include(message)
+	})
 })
