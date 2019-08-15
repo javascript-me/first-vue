12caedb7ddb73d5be986fd0986e9aabadb385113
agileabc <agileabc@sina.com>
2019-08-15 17:09:39 +0800

    Rename.

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,6 @@
 import { expect, assert } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/hello-world.vue'
 
 describe('HelloWorld.vue', () => {
 	it('renders props.msg when passed', () => {
